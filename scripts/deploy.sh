1eb57788ee5abe9af31f8ede73752118ba583ef7
Demis Bellot <demis.bellot@gmail.com>
2016-10-27 04:52:42 -0400

    fix condition

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,7 +17,9 @@ envsubst < task-definition.json > new-task-definition.json
 eval $(aws ecr get-login --region $AWS_DEFAULT_REGION) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
 
 if [ $(aws ecr describe-repositories | jq --arg x $IMAGE_NAME '[.repositories[] | .repositoryName == $x] | any') == "true" ]; then
-    echo "ECS Repository doesn't' exist, Creating $IMAGE_NAME ..."
+    echo "Found ECS Repository $IMAGE_NAME"
+else
+    echo "ECS Repository doesn't exist, Creating $IMAGE_NAME ..."
     aws ecr create-repository --repository-name $IMAGE_NAME
 fi
 
